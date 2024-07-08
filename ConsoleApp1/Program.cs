966bacb0219ed0a73e07feaa376642c0ba1d6a8b
Lariyan.Pouya <Lariyan.Pouya@HIG001P4987>
2024-07-08 11:24:58 +0330

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿var intList = Enumerable.Range(1, 20).Select(x => IntGenerator()).ToList();
+
+
+////////// remove comment and test it 
+/*Console.WriteLine("---------- All at once ---------");
+var result = await Task.WhenAll(intList);   
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
+*/
+
+Console.WriteLine("-------- one by one -------------");
+while (intList.Any())
+{
+    var intGenerated = await Task.WhenAny(intList);
+    intList.Remove(intGenerated);
+    Console.WriteLine(await intGenerated);
+}
+
+
+/*
+// .net 9
+await foreach(var item in Task.WhenEach(intList))
+{
+    Console.WriteLine(await item);  
+}
+*/
+
+async Task<int> IntGenerator()
+{
+    var intNum = new Random().Next(100, 1000);
+    await Task.Delay(intNum);
+    return intNum;
+}
